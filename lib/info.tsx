5965b335c96e42b0fb1b6728690e1c526cd2dea9
Markus Trachsel <markus@codechefs.ch>
2023-03-09 14:18:38 +0100

    added name

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,6 +1,6 @@
-import me from '../app/placeholder.jpg';
+import me from '../app/profile.jpg';
 
-export const name = 'Your Name';
+export const name = 'Markus Trachsel';
 export const avatar = me;
 export const about = () => {
   return (
