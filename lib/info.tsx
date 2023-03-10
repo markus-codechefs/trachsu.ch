1fbf4f2e4eff4df13d7981e2c1c7e5f05e88f7f1
Markus Trachsel <markus@codechefs.ch>
2023-03-10 10:02:00 +0100

    Updated information text

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,14 +5,14 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      As a software architect and full-stack engineer, I bring a passion for empowering agile software teams through DevOps principles, small batch sizes, and frequent deployments. My approach prioritizes people over technology, recognizing the vital role software plays in supporting and improving organizational goals. My expertise extends to process automation and integration and pipelines, which allows me to help clients optimize their productivity and efficiency through the use of appropriate technology. 
+      As a full-stack engineer, I bring a passion for empowering agile software teams through DevOps principles, small batch sizes, and frequent deployments. My approach prioritizes people over technology. My expertise extends to process automation and integration and pipelines, which allows me to help clients optimize their productivity and efficiency through the use of appropriate technology. 
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      Beyond work, I spend time with my familiy, I also enjoy playing basketball and exploring my culinary creativity in the kitchen.
+      Beyond work, I spend time with my familiy, enjoy playing basketball and love exploring my culinary creativity in the kitchen. 
     </>
   );
 };
