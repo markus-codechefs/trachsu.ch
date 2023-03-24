03159b547ebe2417e1d279610da320b20ed89888
Markus Trachsel <markus@codechefs.ch>
2023-03-24 11:17:13 +0100

    Fixed twitter name

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -69,7 +69,7 @@ export const getTweets = async (ids) => {
 
 export const getTweetCount = async () => {
   const response = await fetch(
-    `https://api.twitter.com/2/users/by/username/markus_trachsel?user.fields=public_metrics`,
+    `https://api.twitter.com/2/users/by/username/trachsel_markus?user.fields=public_metrics`,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_API_TOKEN}`,
