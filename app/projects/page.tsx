30b970a9ce5d7e1afb0bea9340c318f994c92546
Markus Trachsel <markus@codechefs.ch>
2023-04-06 21:34:45 +0200

    Made dark card background neutral

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,11 +5,11 @@ export default function ProjectsPage() {
     <section>
       <h1 className="font-bold text-3xl font-serif mb-5">Projects</h1>
       <div className="grid grid-cols-2 gap-4 my-4">
-      <div>
+        <div>
           {project(
             "trachsu.ch",
             "My landing page which gave me ample opportunity to dive into the world of react, nextjs and vercel",
-            ["react", "next.js", "type script", "vercel","planetscale db"]
+            ["react", "next.js", "type script", "vercel", "planetscale db"]
           )}
         </div>
         <div>
@@ -18,10 +18,10 @@ export default function ProjectsPage() {
             "I created a web portal using .NET Core, Orchard.net CMS, Bootstrap, and MS-SQL. It was hosted on Azure and deployed with Azure DevOps.",
             [".NET Core", "Bootstrap", "ms-sql"]
           )}
-        </div>        
+        </div>
       </div>
       <div className="grid grid-cols-2 gap-4">
-      <div>
+        <div>
           {project(
             "Increased deployment frequency at immoscout24.ch",
             "Applying the DORA metrics and modern practices like CI /CD we increased the deployment frequency to production from 4 times per month to 200!",
@@ -33,7 +33,7 @@ export default function ProjectsPage() {
             "Automated Personal Debtcheck API from Intrum Justitia AG for Meisterwerk GmbH",
             "Analysis, design, implementation and testing of the API. Integration and automation of the ordering process at the customer.",
             ["Shopify API", "C#", ".NET Core", "Rest API"]
-          )}          
+          )}
         </div>
       </div>
     </section>
--- components/project.tsx
+++ components/project.tsx
@@ -1,7 +1,7 @@
 export default function project(title: string, text: string, tags: string[]) {
   return (
     <>
-      <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-neutral-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
